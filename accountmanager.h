c27b613da84472f115e9fea4a797436117c269a0
Ralph Julsaint <rjpro81@hotmail.com>
2020-10-04 22:33:48 -0400

    Reformat code to use CSV flat files for password storage

--- accountmanager.h
+++ accountmanager.h
@@ -1,24 +1,6 @@
 #ifndef ACCOUNTMANAGERHEADER
 #define ACCOUNTMANAGERHEADER
 
-int getLoginResult(void *data, int argc, char **argv, char **col);
 
-int getPasswords(void *data, int argc, char **argv, char **col);
-
-int getMasterId(char *mPass);
-
-int getUserPassword(int userId);
-
-int createMasterPassword(const char *masterPass);
-
-int createLogin(char *mPass);
-
-int createAccount(void);
-
-int deleteAccount(void);
-
-int accountLogin(char *username, char *password, int id);
-
-char* substring(char *str, int startIndex, int endIndex);
 
 #endif
--- passwordgenerator.h
+++ passwordgenerator.h
@@ -4,10 +4,6 @@
 
 char* defaultLengthPassword(void);
 char* variedLengthPassword(int);
-int savePassword(int, char*, char*);
-int getPasswords(void *, int, char **, char **);
-void getPassword(char *description);
-int getPasswordsView(void);
-int deletePassword(int);
+
 
 #endif
